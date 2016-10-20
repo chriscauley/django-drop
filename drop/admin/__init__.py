39590b5b5cfcdcd9cb244dff038e45e76e46c105
chriscauley <chris@lablackey.com>
2016-10-19 20:48:44 -0400

    adding category admin (from thegamesupply)

--- drop/admin/__init__.py
+++ drop/admin/__init__.py
@@ -1,2 +1,11 @@
 # flake8: noqa
+from django.contrib import admin
+
+from drop.models import Category
 import orderadmin
+
+from lablackey.db.admin import NamedTreeModelAdmin
+
+@admin.register(Category)
+class CategoryAdmin(NamedTreeModelAdmin):
+  pass
