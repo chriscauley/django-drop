cc05ae7a5b802223ee22330acec370a7ca487e59
chriscauley <chris@lablackey.com>
2016-12-21 12:24:23 -0500

    changing fields and delivery method for giftcard entirely

--- drop/giftcard/models.py
+++ drop/giftcard/models.py
@@ -16,9 +16,11 @@ import datetime, random, jsonfield, arrow
 class GiftCardProduct(Product,PhotosMixin):
   has_quantity = False
   in_stock = 1e6
-  extra_fields = ['recipient_name','recipient_email','delivery_date','amount']
+  extra_fields = ['to','from','amount']
   class Meta:
     app_label = "giftcard"
+  def purchase_message(self):
+    return "Information on how to print and redeem your gift card will be sent to your email address. Be sure to check your spam folder if you do not see this email in the next 10 minutes."
   def purchase(self,cart_item):
     user = cart_item.order.user
     quantity = cart_item.quantity
@@ -28,22 +30,21 @@ class GiftCardProduct(Product,PhotosMixin):
       amount=quantity,
       product=self,
       extra=cart_item.extra,
-      delivery_date=arrow.get(cart_item.extra['delivery_date'],'M/D/YYYY').date(),
     )
-    if credit.delivery_date <= datetime.date.today():
-      credit.send()
+    credit.send()
 
 class Credit(models.Model,JsonMixin):
   code = models.CharField(max_length=16)
   created = models.DateTimeField(auto_now_add=True)
   purchased_by = models.ForeignKey(settings.AUTH_USER_MODEL,related_name="+")
   user = models.ForeignKey(settings.AUTH_USER_MODEL,null=True,blank=True)
-  delivery_date = models.DateField()
-  delivered = models.DateTimeField(null=True,blank=True)
   product = models.ForeignKey(GiftCardProduct)
   amount = CurrencyField()
   extra = jsonfield.JSONField(default=dict,null=True,blank=True)
   json_fields = ['created','remaining','code','extra']
+
+  def get_image_url(self):
+    return reverse("giftcard_image",args=[self.code])
 
   def get_absolute_url(self):
     url = getattr(settings,"DROP_GIFTCARD_LANDING",None) or reverse("giftcard_redeem")
@@ -53,13 +54,12 @@ class Credit(models.Model,JsonMixin):
     return self.amount - sum(self.debit_set.all().values_list('amount',flat=True))
   __unicode__ = lambda self: self.code
   def send(self):
-    # Don't resend or send if already redeemed
-    if self.delivered or self.user:
-      return
-    to = [self.extra.get('recipient_email',self.purchased_by.email)]
+    to = [self.purchased_by.email]
     context = {'credit': self, 'user_display': self.purchased_by.get_full_name() or self.purchased_by.username}
+    attrs = ['DROP_GIFTCARD_IMG','DROP_GIFTCARD_FONT']
+    context['GIFTCARD_IMAGE'] = all([getattr(settings,a,None) for a in attrs])
+    print all([getattr(settings,a,None) for a in attrs])
     send_template_email("email/send_giftcard",to,context=context)
-    self.delivered  = timezone.now()
     self.save()
 
 class Debit(models.Model,JsonMixin):
