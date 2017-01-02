cbf5ff445bd0d6d5cf21c4e10e61cb01d5fbcc09
chriscauley <chris@lablackey.com>
2017-01-02 11:35:13 -0500

    arst

--- drop/giftcard/models.py
+++ drop/giftcard/models.py
@@ -58,7 +58,7 @@ class Credit(models.Model,JsonMixin):
     context = {'credit': self, 'user_display': self.purchased_by.get_full_name() or self.purchased_by.username}
     attrs = ['DROP_GIFTCARD_IMG','DROP_GIFTCARD_FONT']
     context['GIFTCARD_IMAGE'] = all([getattr(settings,a,None) for a in attrs])
-    send_template_email("email/send_giftcard",to,context=context,bcc=['chris@lablackey.com'])
+    send_template_email("email/send_giftcard",to,context=context,bcc=['cauley.chris@gmail.com'])
     self.save()
 
 class Debit(models.Model,JsonMixin):
