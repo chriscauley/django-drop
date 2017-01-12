76ee607197847ecbe17c836b0ec8a7045261fb64
chriscauley <chris@lablackey.com>
2017-01-12 13:13:15 -0500

    add cart details to stripe payment

--- drop/payment/backends/stripe_backend.py
+++ drop/payment/backends/stripe_backend.py
@@ -10,12 +10,17 @@ from .abstract import PaymentBackend
 class Stripe(PaymentBackend):
   name = "stripe"
   def charge(self,order,request):
+    metadata = {"order_id": order.id}
+    for i,item in enumerate(order.items.all()):
+      metadata['item_%s'%i] = str(item.product)
+      metadata['quantity_%s'%i] = item.quantity
+      metadata['price_%s'%i] = item.line_total
     kwargs = {
       'amount': int(float(request.POST['total'])*100), # Amount in cents
       'source': request.POST['token'],
       'currency': "usd",
       'description': "Payment for order #%s"%order.id,
-      'metadata': {"order_id": order.id},
+      'metadata': metadata,
     }
     customer = None
     card = None
