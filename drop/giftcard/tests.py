661e96e7b90651fac22880b24b1f82bbb0b2e4ed
chriscauley <chris@lablackey.com>
2017-01-25 17:40:13 -0500

    giftcard tests complete!

--- drop/giftcard/tests.py
+++ drop/giftcard/tests.py
@@ -1,3 +1,65 @@
-from django.test import TestCase
+from django.core.urlresolvers import reverse
 
-# Create your tests here.
+from drop.giftcard.models import GiftCardProduct, Credit
+from drop.models import Order, Product
+from drop.payment.api import PaymentAPI
+from drop.test_utils import DropTestCase, print_order
+
+from decimal import Decimal
+
+class GiftCardTestCase(DropTestCase):
+  def test_giftcard_purchase(self):
+    gcp = GiftCardProduct.objects.create(name="gift card",unit_price=1)
+    product40 = self.new_product(unit_price=40)
+    product20 = self.new_product(unit_price=20)
+    user1 = self.new_user()
+
+    # user1 buys a giftcard
+    self.login(user1)
+    order_id = self.add_to_cart(gcp,50,extra={'to': 'jon','from': 'jan'})
+    items = user1.cart.items.all()
+    self.assertEqual(items.count(),1)
+    self.assertEqual(items[0].extra['to'],'jon')
+    self.assertEqual(items[0].extra['from'],'jan')
+    order = Order.objects.get(id=order_id)
+    #print_order(order)
+    PaymentAPI().confirm_payment(order,order.order_total,"fake transaction","fake backend","fake description")
+    credit = Credit.objects.get(purchased_by=user1)
+
+    # user2 redeems giftcard
+    user2 = self.new_user()
+    self.login(user2)
+    giftcard = self.client.post(reverse("giftcard_redeem_ajax"),{'code':credit.code}).json()['giftcard']
+    self.assertEqual(giftcard['remaining'],'50.00')
+
+    # user2 buys an entire cart using a giftcard
+    order2_id = self.add_to_cart(product40)
+    payment_url = reverse("drop_payment",args=["giftcard"])
+    response = self.client.post(payment_url,{'code': credit.code,'total': 40})
+    self.assertTrue(response.json()['next'].startswith('/thank_you/%s/?token='%order2_id))
+    order2 = Order.objects.get(pk=order2_id)
+    #print_order(order2)
+    self.assertTrue(order2.is_paid())
+
+    # gift card now says less money
+    giftcard = self.client.post(reverse("giftcard_redeem_ajax"),{'code':credit.code}).json()['giftcard']
+    self.assertEqual(giftcard['remaining'],'10.00')
+
+    # user2 partially pays for a cart using a giftcrd
+    order3 = Order.objects.get(pk=self.add_to_cart(product20))
+    #print_order(order3)
+
+    # Two partial payments work and take order cost down to $10
+    response1 = self.client.post(payment_url,{'code': credit.code,'total': 5})
+    response2 = self.client.post(payment_url,{'code': credit.code,'total': 5})
+    self.assertEqual(len(response2.json()['extra_price_fields']),2)
+    order3 = Order.objects.get(pk=order3.id)
+    self.assertEqual(order3.amount_paid,Decimal(10))
+
+    # Third fails because balance on gift card is zero
+    response3 = self.client.post(payment_url,{'code': credit.code,'total': 5})
+    self.assertTrue('Your gift card balance is $0.00' in response3.json()['error'])
+
+    # user2 fully pays with another payment method
+    PaymentAPI().confirm_payment(order3,10,"fake transaction","fake backend","fake description")
+    self.assertTrue(Order.objects.get(pk=order3.id).is_paid())
--- drop/giftcard/urls.py
+++ drop/giftcard/urls.py
@@ -6,7 +6,7 @@ import views
 urlpatterns = [
   url(r'^redeem/$',lablackey.views.single_page_app,name="giftcard_redeem"),
   url(r'^user.json$',views.user_json),
-  url(r'^redeem_ajax/$',views.redeem_ajax),
+  url(r'^redeem_ajax/$',views.redeem_ajax,name="giftcard_redeem_ajax"),
   url(r'^validate/$',views.validate),
   url(r'^([\d\w]+).png$',views.image,name="giftcard_image"),
   url(r'^([\d\w]+).jpg$',views.image), #just in case
