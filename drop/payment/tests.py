fdabb19fe4411ab0306f4b89ee8f42a3a05364c9
chriscauley <chris@lablackey.com>
2017-01-25 17:41:29 -0500

    payment tests updated

--- drop/payment/tests.py
+++ drop/payment/tests.py
@@ -1,20 +1,10 @@
-from drop.test_utils import DropTestCase
-
 from drop.backends_pool import payment_backends
 from drop.models import Order
 from drop.payment.api import PaymentAPI
+from drop.test_utils import DropTestCase, print_order
 
 from decimal import Decimal
 import random
-
-def print_order(order_id):
-  print "-------------\nOrder #%s"%order_id
-  order = Order.objects.get(pk=order_id)
-  print "total: %s"%order.order_total
-  for i in order.items.all():
-    print i,'  ',i.line_total
-  for op in order.orderpayment_set.all():
-    print "payment: %s"%op.amount
 
 class PaymentModuleTestCase(DropTestCase):
   def setUp(self):
@@ -26,12 +16,12 @@ class PaymentModuleTestCase(DropTestCase):
     # Add an item to cart
     order_id = self.add_to_cart(self.product10)
     order = Order.objects.get(pk=order_id)
-    print_order(order_id)
+    #print_order(order_id)
     self.assertEqual(order.items.count(),1)
     self.assertEqual(order.order_total,10)
 
     # Add another one but we're using the above un-updated order for the payment
     order2_id = self.add_to_cart(self.product2)
-    print_order(order2_id)
     PaymentAPI().confirm_payment(order,Decimal(10),"faketransaction%s"%random.random(),'paypal',"Faked paypal payment")
-    order = Order.objects.get(pk=order_id)
+    #print_order(order_id) #passing in id causes it to re look up order
+    #print_order(order2_id)
