34dbc75798df55415d535e0e86ecd015d3b1a903
chriscauley <chris@lablackey.com>
2017-01-25 11:44:47 -0500

    split add_to_cart and start_checkout for more fine grained control

--- drop/test_utils.py
+++ drop/test_utils.py
@@ -6,11 +6,14 @@ from drop.models import Product
 import random, decimal
 
 class DropTestCase(ClientTestCase):
-  def add_to_cart(self,product,quantity=1):
+  def add_to_cart(self,product,quantity=1,start_checkout=True):
     self.client.post(reverse('cart_edit'),{
       'id': product.id,
       'quantity': quantity
     })
+    if start_checkout:
+      return self.start_checkout()
+  def start_checkout(self):
     _r = self.client.get(reverse('start_checkout'))
     return _r.json()['order_id']
   def new_product(self,name=None,unit_price=1):
