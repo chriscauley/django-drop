0c45e11fd93877570489f220ad56487d5739dffc
chriscauley <chris@lablackey.com>
2017-02-01 13:14:53 -0500

    adding direct link to promocode

--- drop/discount/admin.py
+++ drop/discount/admin.py
@@ -1,3 +1,4 @@
+from django.conf import settings
 from django.contrib import admin
 
 from .models import ProductDiscount, Promocode, PromocodeUsage
@@ -8,7 +9,11 @@ class ProductDiscountAdmin(admin.ModelAdmin):
 
 @admin.register(Promocode)
 class PromocodeAdmin(admin.ModelAdmin):
-  pass
+  readonly_fields = ("direct_link",)
+  def direct_link(self,obj):
+    url = "%s?p=%s"%(settings.SITE_URL,obj.code)
+    return "<a href='%s'>%s</a><br/>%s"%(url,obj.name,url)
+  direct_link.allow_tags = True
 
 @admin.register(PromocodeUsage)
 class PromocodeUsageAdmin(admin.ModelAdmin):
